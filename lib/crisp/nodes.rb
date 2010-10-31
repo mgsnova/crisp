dfaf45146199b8b7038e564634ebc93d2015ff41
mgsnova <github@mgsnova.de>
2010-10-31 14:18:42 +0100

    minor code refactoring

--- lib/crisp/nodes.rb
+++ lib/crisp/nodes.rb
@@ -1,13 +1,25 @@
 module Crisp
   class Base < Treetop::Runtime::SyntaxNode
     def eval(env)
-      env
+      nil
     end
   end
 
   class Operation < Base
     def eval(env)
       env[self.func_identifier.text_value].eval(env, self.list.elements.collect(&:parameter))
+    end
+  end
+
+  class Block < Base
+    def eval(env)
+      last_result = nil
+
+      elements.each do |op|
+        last_result = op.eval(env)
+      end
+
+      last_result
     end
   end
 
@@ -35,18 +47,6 @@ module Crisp
   class Symbol < Primitive
     def internal_value(env)
       text_value.to_sym
-    end
-  end
-
-  class Block < Base
-    def eval(env)
-      last_result = nil
-
-      elements.each do |op|
-        last_result = op.eval(env)
-      end
-
-      last_result
     end
   end
 end
