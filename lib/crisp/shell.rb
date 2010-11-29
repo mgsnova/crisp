ccea0b6dac2678bcafa921f0e7ab98832fa5498e
mgsnova <github@mgsnova.de>
2010-11-29 20:10:51 +0100

    improve syntax error handling in shell

--- lib/crisp/shell.rb
+++ lib/crisp/shell.rb
@@ -4,12 +4,16 @@ module Crisp
 
     def run
       runtime = Runtime.new
+      buffer = ''
 
-      while (line = Readline.readline(">> ", true))
-        if ast = Parser.new.parse(line)
+      while (line = Readline.readline(buffer.empty? ? ">> " : "?> ", true))
+        begin
+          buffer << line
+          ast = Parser.new.parse(buffer)
           puts "=> " + runtime.run(ast).to_s
-        else
-          print "?> "
+          buffer = ''
+        rescue Crisp::SyntaxError => e
+          # noop
         end
       end
     end
