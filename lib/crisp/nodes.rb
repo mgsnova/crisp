514b54823bfae844545a2b3fe2f9661893029ec2
mgsnova <github@mgsnova.de>
2010-12-11 15:06:33 +0100

    rename number node file to integer, more doc

--- lib/crisp/nodes.rb
+++ lib/crisp/nodes.rb
@@ -1,13 +1,28 @@
 module Crisp
   module Nodes
     # require all used node types
+    #
+    # hierarchy:
+    #
+    #   Base
+    #   + Operation
+    #   + Block
+    #   + Primitive
+    #     + Array
+    #     + Integer
+    #     + Float
+    #     + String
+    #     + Symbol
+    #     + True
+    #     + False
+    #     + Nil
 
     require 'crisp/nodes/base'
     require 'crisp/nodes/operation'
     require 'crisp/nodes/block'
     require 'crisp/nodes/array_literal'
     require 'crisp/nodes/primitive'
-    require 'crisp/nodes/number_literal'
+    require 'crisp/nodes/integer_literal'
     require 'crisp/nodes/float_literal'
     require 'crisp/nodes/string_literal'
     require 'crisp/nodes/symbol_literal'
