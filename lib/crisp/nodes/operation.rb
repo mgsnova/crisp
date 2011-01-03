3b97b4e4429c5a0372545be6659d76f76278d955
mgsnova <github@mgsnova.de>
2011-01-03 21:25:07 +0100

    minor refactoring

--- lib/crisp/nodes/operation.rb
+++ lib/crisp/nodes/operation.rb
@@ -5,15 +5,21 @@ module Crisp
       # get the function binded to the given function name and eval it (including arguments)
       def eval(env)
         if self.func_identifier.class.name == "Crisp::Nodes::Operation"
-          self.func_identifier.eval(env).eval(env, self.element_list.elements.collect(&:element))
+          self.func_identifier.eval(env).eval(env, raw_element_list)
         else
-          env[self.func_identifier.text_value].eval(env, self.element_list.elements.collect(&:element))
+          env[self.func_identifier.text_value].eval(env, raw_element_list)
         end
       end
 
       # an operation resolves to itself
       def resolve(env)
         self
+      end
+
+      private
+
+      def raw_element_list
+        self.element_list.elements.collect(&:element)
       end
     end
   end
